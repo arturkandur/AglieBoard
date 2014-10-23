7b020d19a3096fd3467cd8ac5770b26f95efc3d1
arturkandur <arturkandur@gmail.com>
2014-10-23 23:14:33 +0300

    CHANGE: Task Status as Enum, Refactor

---
+++ src/main/java/com/engagepoint/university/exam/entity/TaskStatus.java
@@ -0,0 +1,30 @@
+package com.engagepoint.university.exam.entity;
+
+/**
+ * Created by arturkandur on 23.10.14.
+ */
+public enum TaskStatus {
+
+    TODO("TODO"),
+    IN_PROGRESS("IN PROGRESS"),
+    ON_QA("ON QA"),
+    DONE("DONE");
+
+    private final String status;
+
+    /**
+     * @param status
+     */
+    private TaskStatus(final String status) {
+        this.status = status;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Enum#toString()
+     */
+    @Override
+    public String toString() {
+        return status;
+    }
+
+}
